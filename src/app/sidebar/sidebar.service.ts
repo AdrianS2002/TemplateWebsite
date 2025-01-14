3cfebf81aedc05a76f4691fb67760060a97123b5
AdrianS2002 <aschiop2002@yahoo.com>
2025-01-14 14:09:38 +0200

    rmoved sidebarbutton

--- src/app/sidebar/sidebar.service.ts
+++ src/app/sidebar/sidebar.service.ts
@@ -5,15 +5,10 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class SidebarService {
-  private isExpandedSubject = new BehaviorSubject<boolean>(false); // Default collapsed state
+  private isExpandedSubject = new BehaviorSubject<boolean>(false);
   isExpanded$ = this.isExpandedSubject.asObservable();
 
-   toggleSidebar() {
-    const currentValue = this.isExpandedSubject.value;
-    this.isExpandedSubject.next(!currentValue); // Toggle the state
-  }
-
-  get isExpanded(): boolean {
-    return this.isExpandedSubject.value;
+  toggleSidebar(): void {
+    this.isExpandedSubject.next(!this.isExpandedSubject.value);
   }
 }
