6bff600f2d1532f6ceae78d4d1e58a0fe9e3b0cc
AdrianS2002 <aschiop2002@yahoo.com>
2025-01-14 12:34:51 +0200

    modified files for accepting footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,27 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
+
+@Component({
+  template:` <h1>Privacy Policy</h1> `,
+  standalone:true
+})
+export class PrivacyPolicyComponent {}
+
+@Component({
+  template:` <h1>Terms of Service</h1> `,
+  standalone:true
+})
+export class TermsOfService {}
+
+
+
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'contact', component: ContactComponent },
+  {path: 'privacy-policy', component: PrivacyPolicyComponent},
+  {path: 'terms-of-service', component: TermsOfService},
   { path: '', redirectTo: '/home', pathMatch: 'full' } // Default route
 ];
