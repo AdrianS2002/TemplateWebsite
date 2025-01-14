86261a5c6710203b9133dc1fb1a8379250a253ef
AdrianS2002 <aschiop2002@yahoo.com>
2025-01-14 13:37:47 +0200

    changed logic in menu component in order to add toggle button

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfigService } from '../config.service';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { SidebarService } from '../sidebar/sidebar.service';
 @Component({
   selector: 'app-menu',
   standalone: true,
@@ -11,15 +12,22 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 })
 export class MenuComponent implements OnInit {
   menuConfig: any;
+  sidebarConfig: any;
+  constructor(private configService: ConfigService, private sidebarService: SidebarService) {}
 
   async ngOnInit(): Promise<void> {
     try {
-      const response = await fetch('template-config.json');
-      const config = await response.json();
-      console.log('Loaded config:', config); // Verifică ce se încarcă
+      const config = await this.configService.getConfig();
+      console.log('Loaded config:', config);
       this.menuConfig = config.menu;
+      this.sidebarConfig = config.sidebar;
+      console.log('Menu config:', this.menuConfig);
     } catch (error) {
       console.error('Error loading menu configuration:', error);
     }
+  }
+
+  toggleSidebar() {
+    this.sidebarService.toggleSidebar(); // Call the service to toggle sidebar state
   }
 }
