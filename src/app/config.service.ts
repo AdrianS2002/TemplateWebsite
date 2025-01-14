c4e975f057ec9959c103b3471e12b6c36c88151f
AdrianS2002 <aschiop2002@yahoo.com>
2025-01-14 13:40:26 +0200

    added service for footer and menu

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -9,9 +9,13 @@ export class ConfigService {
     private configUrl = 'template-config.json';
 
 
-    constructor(private http: HttpClient) {}
-
-    getConfig(): Observable<any> {
-        return this.http.get(this.configUrl);
+    async getConfig(): Promise<any> {
+        try {
+            const response = await fetch(this.configUrl);
+            return await response.json();
+        } catch (error) {
+            console.error('Error loading configuration:', error);
+            throw error;
+        }
     }
 }
