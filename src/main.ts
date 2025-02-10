2bf5d925c3832993ab194c2e424a4169e407a649
AdrianS2002 <aschiop2002@yahoo.com>
2025-02-10 18:32:24 +0200

    added translations for website elements

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,28 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptorService } from './app/auth/auth-interceptor.service';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+// 📌 Definirea manuală a configurației Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAubzKJeHlwP8OHFZvx9NbcAwbfbtsGjdQ",
+  authDomain: "templatewebsite-3e4ee.firebaseapp.com",
+  databaseURL: "https://templatewebsite-3e4ee-default-rtdb.firebaseio.com",
+  projectId: "templatewebsite-3e4ee",
+  storageBucket: "templatewebsite-3e4ee.appspot.com",
+  messagingSenderId: "722715285210",
+  appId: "1:722715285210:web:5fed25b07716267df5f7e3",
+  measurementId: "G-RMHMD8KE9V"
+};
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()), 
-    { provide: AuthInterceptorService, useClass: AuthInterceptorService } 
+    { provide: AuthInterceptorService, useClass: AuthInterceptorService },
+
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ]
-});
+}).catch(err => console.error(err));
